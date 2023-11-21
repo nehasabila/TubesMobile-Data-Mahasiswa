e4f33eca31f2d75e5aa65e6338ecd134a7ccec54
Abdul Hakim <newe6189@gmail.com>
2023-11-21 10:55:44 +0700

    1. Penambahan Mahasiswa di string.xml bagian values 2. Perubahan di MainActivity 3. Nambah Foto

--- app/src/main/java/com/example/tugasmobile/MainActivity.java
+++ app/src/main/java/com/example/tugasmobile/MainActivity.java
@@ -24,14 +24,14 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        int[] imageList = {R.drawable.neha, R.drawable.jauza};
-        int[] nimList = {R.string.nehaNim, R.string.jauzaNim};
-        String[] surelList = {"nehasabila62@gmail.com", "jauzaanbari12@gmail.com"};
-        String[] fakultasList = {"Fasilkom-TI", "Fasilkom-TI"};
-        String[] programbelajarList = {"Teknologi Informasi", "Teknologi Informasi"};
-        int[] semesterterdaftarList = {R.string.nehaSemesterterdaftar, R.string.jauzaSemesterterdaftar};
-        String[] statusList = {"Aktif", "Aktif"};
-        String[] nameList = {"Neha Sabila Nazmira Sitorus", "Jauza Hayah Anbari"};
+        int[] imageList = {R.drawable.neha, R.drawable.jauza, R.drawable.hakim, R.drawable.erastus, R.drawable.yusuf, R.drawable.david, R.drawable.anhar, R.drawable.caesto, R.drawable.fairuz, R.drawable.ikram};
+        int[] nimList = {R.string.nehaNim, R.string.jauzaNim, R.string.hakimNim, R.string.erastusNim, R.string.yusufNim, R.string.davidNim, R.string.anharNim, R.string.caestoNim, R.string.fairuzNim, R.string.ikramNim};
+        String[] surelList = {"nehasabila62@gmail.com", "jauzaanbari12@gmail.com", "abdulhakim78@gmail.com","erastus42@gmail.com","yusuf60@gmail.com","david141@gmail.com","anhar42@gmail.com","caesto84@gmail.com","fairuz48@gmail.com","ikram108@gmail.com"};
+        String[] fakultasList = {"Fasilkom-TI", "Fasilkom-TI","Fasilkom-TI", "Fasilkom-TI","Fasilkom-TI", "Fasilkom-TI","Fasilkom-TI", "Fasilkom-TI","Fasilkom-TI", "Fasilkom-TI"};
+        String[] programbelajarList = {"Teknologi Informasi", "Teknologi Informasi","Teknologi Informasi", "Teknologi Informasi","Teknologi Informasi", "Teknologi Informasi","Teknologi Informasi", "Teknologi Informasi","Teknologi Informasi", "Teknologi Informasi"};
+        int[] semesterterdaftarList = {R.string.nehaSemesterterdaftar, R.string.jauzaSemesterterdaftar,R.string.nehaSemesterterdaftar, R.string.jauzaSemesterterdaftar,R.string.nehaSemesterterdaftar, R.string.jauzaSemesterterdaftar,R.string.nehaSemesterterdaftar, R.string.jauzaSemesterterdaftar,R.string.nehaSemesterterdaftar, R.string.jauzaSemesterterdaftar};
+        String[] statusList = {"Aktif", "Aktif","Aktif", "Aktif","Aktif", "Aktif","Aktif", "Aktif","Aktif", "Aktif"};
+        String[] nameList = {"Neha Sabila Nazmira Sitorus", "Jauza Hayah Anbari", "Abdul Hakim Rahmad Nasution", "Erastus Keytaro Bangun", "Yusuf Valentino Pandiangan", "David Stephan Bangun", "Al Anhar Sufi", "Caesto Marco Tampubolon", "Fairuz Arifin Basmara Helmie Marpaung", "Muhammad Ikram Al-Asyari Lubis"};
 
         for (int i = 0; i < imageList.length; i++){
             listData = new ListData(nameList[i], nimList[i], imageList[i], surelList[i], fakultasList[i], programbelajarList[i], semesterterdaftarList[i], statusList[i]);
